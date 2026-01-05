0f784f0a3ba082673f7e40df35dd1025e668f7a1
Riwaj Chipalu <69961549+Reewaz-C@users.noreply.github.com>
2026-01-05 02:44:04 +0000

    added pipeline

---
+++ deploy.groovy
@@ -0,0 +1,49 @@
+pipeline {
+    agent any
+    
+    parameters {
+        string(
+            name: 'SERVER_IP',
+            defaultValue: '44.198.182.248',
+            description: 'Enter server IP address'
+        )
+    }
+    
+    environment {
+        SSH_KEY_ID = 'EC2_SSH_KEY'
+    }
+    
+    triggers {
+        githubPush()
+    }
+    
+    stages {
+    
+        stage('Configure SSH') {
+            steps {
+                sh '''
+                mkdir -p ~/.ssh
+                chmod 700 ~/.ssh
+                cat <<EOF > ~/.ssh/config
+Host *
+    StrictHostKeyChecking no
+EOF
+                chmod 600 ~/.ssh/config
+                touch ~/.ssh/known_hosts
+                chmod 600 ~/.ssh/known_hosts
+                '''
+            }
+        }
+        stage('Deploy') {
+            steps {
+                sshagent(['EC2_SSH_KEY']) {
+                    sh '''
+                    rsync -av --delete --exclude='.git' -e "ssh -p 22" ./ ubuntu@${SERVER_IP}:/var/www/html/        
+                    
+                    ssh -p 22 ubuntu@${SERVER_IP} 'sudo systemctl restart nginx'
+                    '''
+                }
+            }
+        }
+    }
+}
