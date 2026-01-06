c5d2544eda4352e35cfbbe1a1c1dd63d94e54e36
Riwaj Chipalu <69961549+Reewaz-C@users.noreply.github.com>
2026-01-06 11:22:02 +0000

    Update: Jenkins file

--- deploy.groovy
+++ deploy.groovy
@@ -49,9 +49,7 @@ pipeline {
                 sshagent(['EC2_SSH_KEY']) {
                     sh """
                         ssh -p 22 -o StrictHostKeyChecking=no ubuntu@${SERVER_IP} '
-                        rm -rf ~/node-app || true
-                        git clone -b Docker-build https://github.com/Reewaz-C/Jenkins.git ~/node-app
-                        cd ~/node-app
+                        git pull origin Docker-build
                         sudo docker compose down || true
                         sudo docker compose up -d --build
                         '
